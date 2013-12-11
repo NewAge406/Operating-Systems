38729a042168a046620059fee25161399a553a80
NewAge406 <Kundig@gmail.com>
2013-12-11 06:11:01 -0700

    Dec 11 changes

--- Project8/myShell.c
+++ Project8/myShell.c
@@ -5,12 +5,19 @@
 #include <pwd.h>
 #include <stdbool.h>
 
+/*Tobias Kundig
+  section 1
+  Fall '13
+  Project 8 */
+  
+/* Prototypes */ 
+void parseCommand(char *buff);
+
 int main(){
 
   /* Declare instance data*/
   int x;
   char buf[1024];
-  struct passwd* passwd_info;
   
   /*Setup an infinite loop 
     Display a prompt
@@ -26,30 +33,44 @@ int main(){
 
     buf[x-1] = 0;
 
+    /* Start if/else checks*/
     if (buf[0] == '!') {
       system(buf + 1);    
     }
-    else if(strcmp(buf, "uid") == 0){
+    else{parseCommand(buf);}
+  }
+  return 0;
+}
+
+/* After reading the ruberic I created this function to do the
+   logic of figuring IT out*/
+void parseCommand(char *buffer){
+  
+  struct passwd* passwd_info;
+  
+    if(strcmp(buffer, "uid") == 0){
       /*do uid stuff here*/
       printf("Your UID is: %d\n", getuid());
     }
-    else if(strcmp(buf, "user") == 0){
+    else if(strcmp(buffer, "user") == 0){
+      /* Print user information*/
       printf("Current user is: ");
       passwd_info = getpwuid(getuid());
       printf("%s\n", passwd_info->pw_name);
     }
-    else if(strcmp(buf, "quit") == 0){
+    else if(strcmp(buffer, "quit") == 0){
+      /* Exit */
       printf("Thank you come again!\n");
       exit(0);
     }
-    else if(strcmp(buf, "q") == 0){
+    else if(strcmp(buffer, "q") == 0){
+      /* Exit */
       printf("Thank you come again!\n");
       exit(0);
     }
     else{ printf("The shell has only the following cmds:\n");
+      /* Print the shells commands */
       printf(" uid\n user\n quit \n q \n");
       printf("Start with ! to invoke a Unix shell cmd\n");
-    }  
-  }
-  return 0;
+    }
 }
